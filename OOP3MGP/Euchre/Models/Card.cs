f76cea9c54e589cacb94c3fff63a78a4fcec3227
williamd-dc <william.dawson@dcmail.ca>
2025-03-30 13:15:36 -0400

    Constants Added Suit Enum made Card properties added

--- OOP3MGP/Euchre/Models/Card.cs
+++ OOP3MGP/Euchre/Models/Card.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Euchre.Shared;
 
 namespace Euchre.Models
 {
-    internal class Card
+    public class Card
     {
+        #region Class Scope Variables
+        #endregion
+
+        #region Properties
+        public Constants.Suit Suit { get; set; }
+
+        public string Rank { get; set; }
+
+        public bool IsTrump { get; set; }
+
+        #endregion
     }
 }
---
+++ OOP3MGP/Euchre/Shared/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euchre.Shared
+{
+    public static class Constants
+    {
+        #region Enums
+        public enum Suit { Hearts, Diamonds, Spades, Clubs}
+        #endregion
+    }
+}
