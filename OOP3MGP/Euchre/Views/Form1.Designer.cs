45de2ca5f342f61191be4d69521b2a22b91d2dc4
dylangill1921 <156254542+dylangill1921@users.noreply.github.com>
2025-04-10 17:48:03 -0400

    Updated files

--- OOP3MGP/Euchre/Views/Form1.Designer.cs
+++ OOP3MGP/Euchre/Views/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Euchre
 {
-    partial class Form1
+    partial class frmMainMenu
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "frmMainMenu";
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
